2ec9405228224c69e11efc036e548e8700139517
Sarwarul Rizvi <sarwarul.rizvi@gmail.com>
2015-02-25 15:31:22 +0100

    always copy Global.asax

--- IndianColor.Restaurant.WebUI/Controllers/HomeController.cs
+++ IndianColor.Restaurant.WebUI/Controllers/HomeController.cs
@@ -20,14 +20,14 @@ namespace IndianColor.Restaurant.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "About us";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact";
 
             return View();
         }
