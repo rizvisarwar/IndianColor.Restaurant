8ad4050e0036fe31450d936fcc6e4ff8895dba3f
stpriyanka <stpriyanka2011@gmail.com>
2015-04-16 14:46:31 +0200

    adjust carousel setting to home

--- IndianColor.Restaurant.WebUI/IndianColorEF/Category.cs
+++ IndianColor.Restaurant.WebUI/IndianColorEF/Category.cs
@@ -8,16 +8,10 @@ namespace IndianColor.Restaurant.IndianColorEF
 {
     public class Category
     {
-        //public Category()
-        //{
-        //    this.FoodMenuItems = new HashSet<FoodMenuItem>();
-        //}
-
         [Key]
         public string Name { get; set; }
         public string Description { get; set; }
 
-        //public virtual ICollection<FoodMenuItem> FoodMenuItems { get; set; }
-        //test to git push
+        
     }
 }
