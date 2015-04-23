0035827999b1bf89f3690ea7c0c55474b04bb5ed
Sarwarul Rizvi <sarwarul.rizvi@gmail.com>
2015-04-23 09:39:08 +0200

    added 'MakeActive' function back, as it is still in use.

--- IndianColor.Restaurant.WebUI/Scripts/Utils.js
+++ IndianColor.Restaurant.WebUI/Scripts/Utils.js
@@ -1,3 +1,18 @@
 ﻿var Utils = {
-
+    MakeActive: function (title) {
+        var selector = '.nav li';
+        $(selector).removeClass('active');
+        if (title == "About") {
+            $('#navAbout').addClass('active');
+        }
+        else if (title == "Home") {
+            $('#navHome').addClass('active');
+        }
+        else if (title == "Reservations") {
+            $('#navReservations').addClass('active');
+        }
+        else if (title == "Contact") {
+            $('#navLocation').addClass('active');
+        }
+    },
 }
