181816762d1d049658b037a5567d6e922de2b3d2
stpriyanka <stpriyanka2011@gmail.com>
2015-05-06 15:03:25 -0700

    enabled login with fb & google, created DB , add map on contact , activate search by category

---
+++ Ecomerce_Restaurant/Ecomerce_Restaurant/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Ecomerce_Restaurant.Models.FoodModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecomerce_Restaurant.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        FoodModelsDB db = new FoodModelsDB();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult SearchView(string searchvalue)
+        {
+
+            var v = db.FoodNamesTable.Where(r => r.CategoryName == searchvalue).ToList();
+            return View(v);
+        }
+    }
+}
--- IndianColor.Restaurant.WebUI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IndianColor.Restaurant.MvcApplication" Language="C#" %>
